bf682f01f2baaa9cb4c5b8085bc827a526a51c01
Mary Han <maryzhenghan@gmail.com>
2018-04-23 16:24:09 -0400

    Add CRUD functionality for app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostRouter = require('/blogPostRouter');
+
+// to log the HTTP layer, use morgan
+app.use(morgan('common'));
+
+// reroute requests that come into `/blog-posts
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
