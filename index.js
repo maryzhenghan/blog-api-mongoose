d45ba85a448f29f619fb9a60496c67c0089f8857
Mary Han <maryzhenghan@gmail.com>
2018-04-23 16:36:05 -0400

    Fix error in naming

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const morgan = require('morgan');
 
 const app = express();
 
-const blogPostRouter = require('./blogPostRouter');
+const blogPostRouter = require('./blogPostsRouter');
 
 // to log the HTTP layer, use morgan
 app.use(morgan('common'));
 app.use(express.static('public'));
 
 // reroute requests that come into `/blog-posts
-app.use('/blog-posts', blogPostRouter);
+app.use('/blog-posts', blogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
