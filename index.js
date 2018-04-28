842cc6aa2143693c4acd9fdc217ee337f3c53a68
Mary Han <maryzhenghan@gmail.com>
2018-04-27 23:51:12 -0400

    Update runServer and closeServer to mongoose commands

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.static('public'));
 // reroute requests that come into `/blog-posts
 app.use('/blog-posts', blogPostsRouter);
 
+
 // runServer and closeServer need to access the same
 // server object, so we declare `server` here, and when
 // runServer runs, it assigns a value
@@ -19,31 +20,36 @@ let server;
 
 // starts our server and returns a Promise.
 // this is how we will asynchronously start our server.
-function runServer() {
-	const port = process.env.PORT || 8080;
+function runServer(databaseUrl, port = PORT) {
 	return new Promise((resolve, reject) => {
+		if (err) {
+			return reject(err);
+		}
 		server = app.listen(port, () => {
 			console.log(`Your app is listening on port ${port}`);
-			resolve(server);
-		}).on('error', err => {
-			reject(err)
+			resolve();
+		})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err)
+			});
 		});
-	});
+	};
 }
 
 // function also needs to return a promise.
 // `server.close` does not return a promise on its own,
 // so we manually create one.
 function closeServer() {
-	return new Promise((resolve, reject) => {
-		console.log('Closing server');
-		server.close(err => {
-			if (err) {
-				reject(err);
-				// so we don't also call `resolve()`
-				return;
-			}
-			resolve();
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
 		});
 	});
 }
@@ -52,7 +58,7 @@ function closeServer() {
 // runs. but we also export the runServer command so other code
 // (for instance, test code) can start the server as needed
 if (require.main === module) {
-	runServer().catch(err => console.error(err));
+	runServer(DATABASE_URL).catch(err => console.error(err));
 };
 
 module.exports = {app, runServer, closeServer};
